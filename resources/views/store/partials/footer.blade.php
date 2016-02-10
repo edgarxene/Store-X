ce15a6aaadc21068a76057946c81874ef46532c9
Edgar Cruz <edgarxene@gmail.com>
2016-02-09 23:05:44 -0600

    VIsta detalle del producto css

---
+++ resources/views/store/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<h3>My laravel store</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et facere cupiditate repudiandae, ea. Ullam temporibus, dolore repellat praesentium, non facilis ad numquam, blanditiis magnam dolorum harum quibusdam aliquam distinctio. Optio cum architecto, facere hic illum doloremque, tempore iste officia quo ab cumque dolorem expedita eius, veritatis. Delectus illo commodi quis.</p>
+		</div>
+		<div class="col-md-4">
+			<h3>Desarrollado por:</h3>
+			<div class="author-info">
+				<img src="http://i.stack.imgur.com/V9dTp.png?s=40&g=1" alt="" class="avatar">
+				<p><a href="">@ergar</a>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, facere. Labore, voluptatem! Totam debitis tenetur, ipsum nihil possimus quidem a repellendus. Consequatur dicta possimus quod culpa. Ratione nostrum praesentium consectetur nihil porro facere, ut, assumenda, voluptatem a, libero voluptates! Tempora.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<h3>Siguenos</h3>
+			<ul class="redes">
+				<li>
+					<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-youtube-square fa-2x"></i></a>
+				</li>
+			</ul>
+			<h3>Escribenos:</h3>
+			<i class="fa fa-at fa-2x"></i><a href="#">ergar@gmail.com</a>
+		</div>
+		<div class="col-md-4">
+			<h3></h3>
+		</div>
+	</div>
+</footer>
--- resources/views/store/template.blade.php
+++ resources/views/store/template.blade.php
@@ -3,10 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('title','StoreX')</title>
+<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
 	@include('store.partials.nav')
 	@yield('content')
 	@include('store.partials.footer')
-</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </html>
