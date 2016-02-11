b13a76a7b9171140e1435a0795ea050ad5f92552
Edgar Cruz <edgarxene@gmail.com>
2016-02-10 22:38:07 -0600

    fonts google

--- resources/views/store/template.blade.php
+++ resources/views/store/template.blade.php
@@ -5,6 +5,7 @@
 	<title>@yield('title','StoreX')</title>
 <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Poiret+One|Lobster+Two' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
